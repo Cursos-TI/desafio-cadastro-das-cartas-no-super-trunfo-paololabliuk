9cdfec2c90a2316a4d11a0a4755325082b8c0fa3
paololabliuk <labliukpaolo@gmail.com>
2025-01-27 19:39:46 +0000

    Apliquei as saídas após entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,18 +7,18 @@ int main() {
     char cidade[20];
     float area;
     float pib;
-    int PT;
+    int pt;
 
-    printf("Digite o codigo da sua carta: \n");
-    scanf("%s", &codigo);
+    printf("Digite o código da sua carta: \n");
+    scanf("%s", codigo);
 
     printf("Digite o estado da sua carta: \n");
-    scanf("%s", &estado);
- 
-    printf("Digite a cidade da sua carta: \n");
-    scanf("%s", &cidade);
+    scanf("%s", estado);
 
-    printf("Digite a populacao da sua carta: \n");
+    printf("Digite a cidade da sua carta: \n");
+    scanf("%s", cidade);
+
+    printf("Digite a população da sua carta: \n");
     scanf("%d", &populacao);
 
     printf("Digite a área em m2 da sua carta: \n");
@@ -28,7 +28,16 @@ int main() {
     scanf("%f", &pib);
 
     printf("Digite quantos pontos turísticos tem sua carta: \n");
-    scanf("%d", &PT);
+    scanf("%d", &pt);
+
+    printf("\n--- Informações da Carta ---\n");
+    printf("Código da carta: %s\n", codigo);
+    printf("Estado da carta: %s\n", estado);
+    printf("Cidade da carta: %s\n", cidade);
+    printf("População da carta: %d\n", populacao);
+    printf("Área da carta: %.2f m2\n", area);
+    printf("PIB da carta: %.2f\n", pib);
+    printf("Pontos turísticos da carta: %d\n", pt);
 
     return 0;
 }
